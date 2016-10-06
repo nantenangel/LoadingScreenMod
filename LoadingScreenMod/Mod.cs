dc9b24b0a441c2f6164547b3b8b64243a2eb6c74
thale5 <thale5@users.noreply.github.com>
2016-10-06 21:31:20 +0300

    Late initialization of the mod options dialog.

--- LoadingScreenMod/Mod.cs
+++ LoadingScreenMod/Mod.cs
@@ -13,7 +13,7 @@ namespace LoadingScreenMod
         public void OnEnabled() => Create();
         public void OnCreated(ILoading loading) { Create(); Util.DebugPrint("OnCreated", Profiling.Millis); }
         public void OnDisabled() => Stopping();
-        public void OnSettingsUI(UIHelperBase helper) => Settings.settings.OnSettingsUI(helper);
+        public void OnSettingsUI(UIHelperBase helper) => Settings.OnSettingsUI(helper);
         public void OnLevelUnloading() { Util.DebugPrint("OnLevelUnloading", Profiling.Millis); }
         public void OnReleased() { Util.DebugPrint("OnReleased", Profiling.Millis); }
 
--- LoadingScreenMod/Settings.cs
+++ LoadingScreenMod/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Xml.Serialization;
 using ColossalFramework.Steamworks;
@@ -34,6 +35,9 @@ namespace LoadingScreenMod
         public bool applyToEuropean = false;
 
         [XmlIgnoreAttribute]
+        bool dirty;
+
+        [XmlIgnoreAttribute]
         internal readonly bool[] skip = new bool[10];
 
         [XmlIgnoreAttribute]
@@ -43,9 +47,11 @@ namespace LoadingScreenMod
         readonly HashSet<string> skippedSet = new HashSet<string>();
 
         public bool SkipAny => Array.Exists(skip, b => b);
-        public bool SkipThis(string name) => skipThese && skippedSet.Contains(name);
+        public bool SkipThis(string name) => skipThese && skippedSet.Contains(name.Trim());
 
         static Settings singleton;
+        static UIHelperBase helper;
+        static bool Dirty => singleton != null && singleton.dirty;
 
         public static Settings settings
         {
@@ -73,20 +79,24 @@ namespace LoadingScreenMod
             }
             catch (Exception) { s = new Settings(); }
 
+            Util.DebugPrint("Settings loaded", Profiling.Millis);
             s.version = 3;
             s.Setup();
             return s;
         }
 
-        public void Save()
+        void Save()
         {
             try
             {
                 Setup();
-                //XmlSerializer serializer = new XmlSerializer(typeof(Settings));
+                dirty = false;
+                XmlSerializer serializer = new XmlSerializer(typeof(Settings));
 
-                //using (StreamWriter writer = new StreamWriter(FILENAME))
-                //    serializer.Serialize(writer, this);
+                using (StreamWriter writer = new StreamWriter(FILENAME))
+                    serializer.Serialize(writer, this);
+
+                Util.DebugPrint("Settings saved", Profiling.Millis);
             }
             catch (Exception e)
             {
@@ -101,40 +111,86 @@ namespace LoadingScreenMod
 
             if (skipThese && !string.IsNullOrEmpty(skippedNames))
                 foreach (string s in skippedNames.Split(delimiters, StringSplitOptions.RemoveEmptyEntries))
-                    skippedSet.Add(s.Trim());
+                {
+                    string t = s.Trim();
+
+                    if (!string.IsNullOrEmpty(t))
+                        skippedSet.Add(t);
+                }
 
             skip[0] = false; skip[1] = skipResLo; skip[2] = skipResHi; skip[3] = skipComLo; skip[4] = skipComHi;
             skip[5] = skipIndGen; skip[6] = skipIndSpe; skip[7] = skipComSpe; skip[8] = skipOffice; skip[9] = skippedSet.Count > 0;
         }
 
-        internal void OnSettingsUI(UIHelperBase helper)
+        static internal void OnSettingsUI(UIHelperBase newHelper)
+        {
+            UIComponent comp = Self(helper);
+
+            if (comp != null)
+                comp.eventVisibilityChanged -= OnVisibilityChanged;
+
+            Profiling.stopWatch.Start();
+            helper = newHelper;
+            comp = Self(newHelper);
+            comp.eventVisibilityChanged -= OnVisibilityChanged;
+            comp.eventVisibilityChanged += OnVisibilityChanged;
+            Util.DebugPrint("OnSettingsUI", Profiling.Millis, "childCount", comp.childCount, "visible", comp.isVisible);
+
+            foreach (StackFrame f in new StackTrace(1).GetFrames())
+            {
+                var method = f.GetMethod();
+                Util.DebugPrint(method.DeclaringType.FullName + "." + method.Name);
+            }
+        }
+
+        static UIComponent Self(UIHelperBase h) => ((UIHelper) h)?.self as UIComponent;
+
+        static void OnVisibilityChanged(UIComponent comp, bool visible)
+        {
+            if (comp == Self(helper))
+            {
+                Util.DebugPrint("OnVisibilityChanged", Profiling.Millis, "childCount", comp.childCount, "visible", visible);
+
+                if (visible && comp.childCount == 0)
+                {
+                    settings.LateSettingsUI(helper);
+                    Util.DebugPrint("SettingsUI created", Profiling.Millis);
+                }
+                else if (!visible && Dirty)
+                    settings.Save();
+            }
+            else
+                Util.DebugPrint("OnVisibilityChanged !!!", Profiling.Millis, "visible", visible);
+        }
+
+        void LateSettingsUI(UIHelperBase helper)
         {
             UIHelper group = CreateGroup(helper, "Loading options for custom assets", "Custom means workshop assets and assets created by yourself");
-            Check(group, "Load enabled assets", "Load the assets enabled in Content Manager", loadEnabled, b => { loadEnabled = b; Save(); });
-            Check(group, "Load used assets", "Load the assets you have placed in your city", loadUsed, b => { loadUsed = b; Save(); });
-            Check(group, "Share textures", "Replace exact duplicates by references", shareTextures, b => { shareTextures = b; Save(); });
-            Check(group, "Share materials", "Replace exact duplicates by references", shareMaterials, b => { shareMaterials = b; Save(); });
-            Check(group, "Share meshes", "Replace exact duplicates by references", shareMeshes, b => { shareMeshes = b; Save(); });
+            Check(group, "Load enabled assets", "Load the assets enabled in Content Manager", loadEnabled, b => { loadEnabled = b; dirty = true; });
+            Check(group, "Load used assets", "Load the assets you have placed in your city", loadUsed, b => { loadUsed = b; dirty = true; });
+            Check(group, "Share textures", "Replace exact duplicates by references", shareTextures, b => { shareTextures = b; dirty = true; });
+            Check(group, "Share materials", "Replace exact duplicates by references", shareMaterials, b => { shareMaterials = b; dirty = true; });
+            Check(group, "Share meshes", "Replace exact duplicates by references", shareMeshes, b => { shareMeshes = b; dirty = true; });
 
             group = CreateGroup(helper, "Skip unused standard buildings  [Experimental]", "This means the buildings included in the base game and DLCs");
-            Check(group, "Residential Low", null, skipResLo, b => { skipResLo = b; Save(); });
-            Check(group, "Residential High", null, skipResHi, b => { skipResHi = b; Save(); });
-            Check(group, "Commercial Low", null, skipComLo, b => { skipComLo = b; Save(); });
-            Check(group, "Commercial High", null, skipComHi, b => { skipComHi = b; Save(); });
-            Check(group, "Commercial Tourism&Leisure", null, skipComSpe, b => { skipComSpe = b; Save(); });
-            Check(group, "Offices", null, skipOffice, b => { skipOffice = b; Save(); });
-            Check(group, "Industry Generic", null, skipIndGen, b => { skipIndGen = b; Save(); });
-            Check(group, "Industry Specialized", null, skipIndSpe, b => { skipIndSpe = b; Save(); });
-            Check(group, "Also skip these named buildings:", "A comma-separated list of building names", skipThese, b => { skipThese = b; Save(); });
+            Check(group, "Residential Low", null, skipResLo, b => { skipResLo = b; dirty = true; });
+            Check(group, "Residential High", null, skipResHi, b => { skipResHi = b; dirty = true; });
+            Check(group, "Commercial Low", null, skipComLo, b => { skipComLo = b; dirty = true; });
+            Check(group, "Commercial High", null, skipComHi, b => { skipComHi = b; dirty = true; });
+            Check(group, "Commercial Tourism&Leisure", null, skipComSpe, b => { skipComSpe = b; dirty = true; });
+            Check(group, "Offices", null, skipOffice, b => { skipOffice = b; dirty = true; });
+            Check(group, "Industry Generic", null, skipIndGen, b => { skipIndGen = b; dirty = true; });
+            Check(group, "Industry Specialized", null, skipIndSpe, b => { skipIndSpe = b; dirty = true; });
+            Check(group, "Also skip these named buildings:", "A comma-separated list of building names", skipThese, b => { skipThese = b; dirty = true; });
             TextField(group, skippedNames, OnSubmitted);
 
             if (Steam.IsOverlayEnabled())
                 Button(group, "Show building names and images using browser", "Opens the Steam web browser", OnWebButton);
 
-            Check(group, "Apply my selections to the European/Vanilla style", "Skip buildings in the built-in district style, too?", applyToEuropean, b => { applyToEuropean = b; Save(); });
+            Check(group, "Apply my selections to the European/Vanilla style", "Skip buildings in the built-in district style, too?", applyToEuropean, b => { applyToEuropean = b; dirty = true; });
 
             group = CreateGroup(helper, "Reports");
-            Check(group, "Save assets report", "Save a report of missing, failed and used assets in " + Util.GetSavePath(), reportAssets, b => { reportAssets = b; Save(); });
+            Check(group, "Save assets report", "Save a report of missing, failed and used assets in " + Util.GetSavePath(), reportAssets, b => { reportAssets = b; dirty = true; });
         }
 
         UIHelper CreateGroup(UIHelperBase parent, string name, string tooltip = null)
@@ -206,7 +262,7 @@ namespace LoadingScreenMod
             {
                 Util.DebugPrint("Submitted: ", text);
                 skippedNames = text;
-                Save();
+                dirty = true;
             }
         }
 
@@ -228,7 +284,7 @@ namespace LoadingScreenMod
 
         void OnWebButton()
         {
-            Steam.ActivateGameOverlayToWebPage("https://cdn.rawgit.com/thale5/Buildings/t1/index.htm");
+            Steam.ActivateGameOverlayToWebPage("https://cdn.rawgit.com/thale5/Buildings/master/toc.htm");
         }
     }
 }
